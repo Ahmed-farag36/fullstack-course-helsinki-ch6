fd816b89a9f597daf1a75f4093fc957ef9abcec8
Ahmed Farag <faragdeveloper@gmail.com>
2020-01-17 15:26:15 +0200

    add redux exercise files to unicafe repo

--- src/index.js
+++ src/index.js
@@ -1,42 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import reducer from "./reducer";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import App from './App'
+import reducer from './reducers/anecdoteReducer'
 
-const store = createStore(reducer);
+const store = createStore(reducer)
 
-const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: "GOOD"
-    });
-  };
-  const ok = () => {
-    store.dispatch({ type: "OK" });
-  };
-  const bad = () => {
-    store.dispatch({ type: "BAD" });
-  };
-  const reset = () => {
-    store.dispatch({ type: "ZERO" });
-  };
+const render = () => {
+  ReactDOM.render(
+    <App store={store} />,
+    document.getElementById('root')
+  )
+}
 
-  return (
-    <div>
-      <button onClick={good}>good</button>
-      <button onClick={ok}>neutral</button>
-      <button onClick={bad}>bad</button>
-      <button onClick={reset}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral {store.getState().ok}</div>
-      <div>bad {store.getState().bad}</div>
-    </div>
-  );
-};
-
-const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById("root"));
-};
-
-renderApp();
-store.subscribe(renderApp);
+render()
+store.subscribe(render)
